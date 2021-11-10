f9d3d4d6d357ec20729f7c0744a6cf625c4f6209
Gerardo Dorado <gerardo.ig.dorado@gmail.com>
2021-11-10 16:19:21 -0300

    Initial Commit

---
+++ flux-employees-api/src/main/java/com/fluxchallenge/fluxemployeesapi/FluxEmployeesApiApplication.java
@@ -0,0 +1,13 @@
+package com.fluxchallenge.fluxemployeesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxEmployeesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxEmployeesApiApplication.class, args);
+	}
+
+}
---
+++ flux-employees-api/src/test/java/com/fluxchallenge/fluxemployeesapi/FluxEmployeesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxchallenge.fluxemployeesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxEmployeesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
