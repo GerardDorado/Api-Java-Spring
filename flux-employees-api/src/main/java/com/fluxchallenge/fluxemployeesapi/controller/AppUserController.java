4f6debf7aaae92db2c319b7831ffd489aaf987cd
Gerardo Dorado <gerardo.ig.dorado@gmail.com>
2021-11-25 10:30:19 -0300

    Add RequestMapping Annotation en AppUserController

--- flux-employees-api/src/main/java/com/fluxchallenge/fluxemployeesapi/controller/AppUserController.java
+++ flux-employees-api/src/main/java/com/fluxchallenge/fluxemployeesapi/controller/AppUserController.java
@@ -7,14 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/AppUser")
+@RestController()
+@RequestMapping("/AppUser")
 public class AppUserController {
 
     @Autowired
     AppUserServiceImpl service;
-
+    
     @GetMapping("/User")
     ResponseEntity<AppUser> getUser(String userName){
         try {
